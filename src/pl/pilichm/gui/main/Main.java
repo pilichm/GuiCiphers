5cb1523bc8fa286e1886437d14842ac16915870e
michalpilichowski <michal.pilichowski@onet.pl>
2023-08-19 16:49:24 +0200

    Init, created project for desktop app for encrypting text.

---
+++ src/pl/pilichm/gui/main/Main.java
@@ -0,0 +1,9 @@
+package pl.pilichm.gui.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main() - start.");
+
+        System.out.println("main() - end.");
+    }
+}
